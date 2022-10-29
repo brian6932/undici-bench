b2689bc2729e9de1aaa7980f21ffdb48fd5cff8c
Brian <18603393+brian6932@users.noreply.github.com>
2022-10-29 00:47:54 -0400

    Added Undici request Minor syntax cleanup Minor performance optimization Updated Undici Fix axios request

--- bench.js
+++ bench.js
@@ -1,32 +1,36 @@
-import nodeFetch from "node-fetch";
-import {fetch as undiciFetch, Agent as UndiciAgent} from "undici";
-import {Agent as HttpsAgent} from "https";
-import pkg from "./1500-deps.json" assert {type: "json"};
-import axios from "axios";
+import nodeFetch from "node-fetch"
+import { fetch as undiciFetch, request as undiciRequest, Agent as UndiciAgent } from "undici"
+import { Agent as HttpsAgent } from "https"
+import pkg from "./1500-deps.json" assert { type: "json" }
+import axios from "axios"
 
-const urls = Object.keys(pkg.devDependencies).map(name => {
-  return `https://registry.npmjs.org/${name.replace(/\//g, "%2f")}`;
-});
+const urls = Object.keys(pkg.devDependencies).map(name =>
+  `https://registry.npmjs.org/${name.replaceAll("/", "%2f")}`
+)
 
-const t1 = performance.now();
-const dispatcher = new UndiciAgent({connections: 96, pipelining: 1});
-await Promise.all(urls.map(async url => {
-  const res = await undiciFetch(url, {dispatcher});
-  await res.text();
-}));
-console.info(`undici: ${Math.round(performance.now() - t1)}ms`);
+const t1 = Date.now()
+const dispatcher = new UndiciAgent({ connections: 96, pipelining: 1 })
+await Promise.all(urls.map(async url =>
+  await (await undiciFetch(url, { dispatcher })).text()
+))
+console.info(`undici fetch: ${Date.now() - t1}ms`)
 
-const t2 = performance.now();
-const agent = new HttpsAgent({maxSockets: 96, keepAlive: true});
-await Promise.all(urls.map(async url => {
-  const res = await nodeFetch(url, {agent});
-  await res.text();
-}));
-console.info(`node-fetch: ${Math.round(performance.now() - t2)}ms`);
+const t2 = Date.now()
+await Promise.all(urls.map(async url =>
+  await (await undiciRequest(url, { dispatcher })).body.text()
+))
+console.info(`undici request: ${Date.now() - t2}ms`)
 
-const t3 = performance.now();
-const httpsAgent = new HttpsAgent({maxSockets: 96, keepAlive: true});
-await Promise.all(urls.map(async url => {
-  const res = await axios.get(url, {httpsAgent, responseType: "text"});
-}));
-console.info(`axios: ${Math.round(performance.now() - t3)}ms`);
+const t3 = Date.now()
+const agent = new HttpsAgent({ maxSockets: 96, keepAlive: true })
+await Promise.all(urls.map(async url =>
+  await (await nodeFetch(url, { agent })).text()
+))
+console.info(`node-fetch: ${Date.now() - t3}ms`)
+
+const t4 = Date.now()
+const httpsAgent = new HttpsAgent({ maxSockets: 96, keepAlive: true })
+await Promise.all(urls.map(async url =>
+  (await axios(url, { httpsAgent, responseType: "text" })).data
+))
+console.info(`axios: ${Date.now() - t4}ms`)
